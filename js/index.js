d9cd1900d92b34e6452c9a0f76457b2a394e7db1
Decco04 <lima23hn@student.ju.se>
2023-11-19 16:46:07 +0100

    home page 1.0
    First version of the home page

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("mousemove", function (e) {
+  var x = (e.clientX - window.innerWidth / 2) / window.innerWidth;
+  var y = (e.clientY - window.innerHeight / 2) / window.innerHeight;
+  document.getElementById("surferImage").style.transform = "translate(" + (-50 + x * 5) + "%, " + (-50 + y * 5) + "%)";
+});
