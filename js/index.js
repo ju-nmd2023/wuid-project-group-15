30c4e6cbe882e6c664c4114ef38bf20b5a3e1223
Decco04 <lima23hn@student.ju.se>
2023-11-30 17:28:43 +0100

    Changed to %

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Inspired by Github copilot: chat
 document.addEventListener("mousemove", function (e) {
   var x = (e.clientX - window.innerWidth / 2) / window.innerWidth;
   var y = (e.clientY - window.innerHeight / 2) / window.innerHeight;
