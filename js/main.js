1e4ebd58b6f5516936734977f3df30e4e84e08dd
ErikSandquist <sandquistarn@gmail.com>
2023-12-01 11:50:17 +0100

    Updated nav with searchbar and css variable names

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ window.addEventListener("scroll", () => {
   let y = window.scrollY;
   if (nav) {
     if (y > 1) {
-      nav.style.backdropFilter = "blur(16px)";
+      nav.style.background = "#f3decc";
+      nav.style.borderBottom = "1px solid #259d9466";
     } else {
-      nav.style.backdropFilter = "blur(0px)";
+      nav.style.background = "#f3decc00";
+      nav.style.borderBottom = "1px solid #259d9400";
     }
   }
 });
